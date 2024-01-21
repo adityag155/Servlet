691e3c607164c597de37beb504702823acdc8cc8
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-21 22:03:45 +0530

    Add files via upload

--- MakeTable2.java
+++ MakeTable2.java
@@ -2,36 +2,35 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 
-public class MakeTable2 extends HttpServlet
-{
-public void doGet(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException
-{
-response.setContentType("text/html");
-PrintWriter out = response.getWriter();
+public class MakeTable2 extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
-out.println("<html>");
-out.println("<body>");
+        out.println("<html>");
+        out.println("<body>");
 
         // Retrieve parameters from the form
-int num1 = Integer.parseInt(request.getParameter("num1"));
-int num2 = Integer.parseInt(request.getParameter("num2"));
+        int num1 = Integer.parseInt(request.getParameter("num1"));
+        int num2 = Integer.parseInt(request.getParameter("num2"));
 
-out.println("<h2>Multiplication Table for " + num1 + " to " + num2 + "</h2>");
-out.println("<table border='1'>");
-out.println("<tr><th>Multiplicand</th><th>Multiplier</th><th>Product</th></tr>");
+        
+        out.println("<table border='2'>");
+        
 
-for (int i = num1; i <= num2; i++) {
-out.println("<tr>");
-for (int j = 1; j <= 10; j++) {
-out.println("<td>" + i + "</td>");
-out.println("<td>" + j + "</td>");
-out.println("<td>" + (i * j) + "</td>");
+        for (int i =1; i <= 10; i++) {
+            out.println("<tr>");
+            for (int j =num1; j <=num2; j++) {
+                
+                out.println("<td>");
+		out.println(i*j);
+            }
+out.println("</td>");
 out.println("</tr>");
-}
-}
+        }
 
-out.println("</table>");
-out.println("</body>");
-out.println("</html>");
-}
+        out.println("</table>");
+        out.println("</body>");
+        out.println("</html>");
+    }
 }
