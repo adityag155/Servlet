b05ba08ff190e4dd6abb9e2ddd704c1c78e603bb
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-21 22:03:28 +0530

    Add files via upload

--- MakeTable.java
+++ MakeTable.java
@@ -2,22 +2,21 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 
-public class MakeTable extends HttpServlet
-{
-public void doGet(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException
-{
-response.setContentType("text/html");
-PrintWriter out=response.getWriter();;
+public class MakeTable extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
-out.println("<html>");
-out.println("<body>");
+        out.println("<html>");
+        out.println("<body>");
 
-out.println("<Form action='MakeTable2'method='post'>");
-out.println("First Number:<input type='text'><br>");
-out.println("second Number:<input type='text'><br>");
-out.println("<input type='submit'name='table'value='Generate Table'>");
+        out.println("<form action='MakeTable2' method='post'>");
+        out.println("First Number: <input type='text' name='num1'><br>");
+        out.println("Second Number: <input type='text' name='num2'><br>");
+        out.println("<input type='submit' name='table' value='Generate Table'>");
 
-out.println("</body>");
-out.println("</html>");
-}
+        out.println("</form>");
+        out.println("</body>");
+        out.println("</html>");
+    }
 }
