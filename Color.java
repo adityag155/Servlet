f2f36984f3c0e89426e28ea990efd898e2a1d1e0
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-29 13:07:34 +0530

    Update Color.java

--- Color.java
+++ Color.java
@@ -4,7 +4,6 @@ import java.io.*;
 
 public class Color extends HttpServlet
 {
-
 public void doGet(HttpServletRequest request,HttpServletResponse response)throws IOException, ServletException
 {
 response.setContentType("text/html");
