85af30911ad80cae307d0376800fb19b0c77df10
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-31 19:28:11 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,12 @@
+public class Product
+{
+  String prname;
+  int price;
+  int qty;
+  int total;
+
+  public String toString()
+   {
+      return prname + " " + price + " " + qty + " " + total;
+   } 
+}
