1e90741534cc95b39b9fb2023b0cea1a8aa0fb62
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-31 19:27:25 +0530

    Add files via upload

--- AddProduct.java
+++ AddProduct.java
@@ -2,23 +2,30 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 
-public class AddProduct extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+public class AddProduct extends HttpServlet 
+{
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
         out.println("<html>");
         out.println("<body>");
 
-        out.println("<form action='Cost' method='get'>");  
-        out.println("Product Name: <input type='text' name='productname'><br>");
-        out.println("Price: <input type='text' name='price'><br>");
-        out.println("Qty: <input type='text' name='qty'><br>");
+        out.println("<form action='Cost'>");
 
-        out.println("<input type='submit' name='Submit'>");
+        out.println("Product Name: <input type='text' name='prname'><br>");
+        out.println("Price: <input type='text' name='price'><br>");
+        out.println("Quantity: <input type='text' name='qty'><br>");
+        out.println("<input type='submit' value='Submit'>");
 
         out.println("</form>");
+
+
         out.println("</body>");
         out.println("</html>");
     }
+
 }
