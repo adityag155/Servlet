ffe5fbb025edcb43756b07a454060f7a9514daa9
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-04-17 22:39:35 +0530

    Add files via upload

---
+++ Session1.java
@@ -0,0 +1,26 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class Session1 extends HttpServlet
+{
+public void deGet(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException
+{
+response.setContentType("text/html"); 
+PrintWriter out=response.getWriter();
+
+out.println("<html>");
+out.println("<body>");
+
+out.println("<form action='Session2'>");
+
+out.println("Name:<input type='text' name='uname'><br>");
+out.println("Password:<input type='password' name='pass'><br>");
+
+out.println("<input type='submit' value='Submit'><br>");
+out.println("</form>");
+
+out.println("</body>");
+out.println("</html>");
+}
+}
